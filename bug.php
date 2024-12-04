de8a37ec2218f3401fe5cad43248f33b1cf3b34c
BugHunter-X <askzenai@gmail.com>
2024-12-04 08:20:05 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,15 @@
+function foo(array $arr) {
+  foreach ($arr as $key => $value) {
+    if ($value === 'bar') {
+      unset($arr[$key]);
+    }
+  }
+  return $arr;
+}
+
+$arr = ['foo', 'bar', 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [0] => foo [2] => baz )
+
+//This code appears correct, but will produce unexpected behavior if the input array is modified within the loop. 
+//This can happen unexpectedly in recursive function calls.
