6a28c6a2e04ee6d4d0ae69659edfb5e76fb32b22
BugHunter-X <askzenai@gmail.com>
2024-12-04 08:20:06 +0100

    Add bugSolution.php file

---
+++ bugSolution.php
@@ -0,0 +1,18 @@
+function foo(array $arr) {
+  $keysToRemove = [];
+  foreach ($arr as $key => $value) {
+    if ($value === 'bar') {
+      $keysToRemove[] = $key;
+    }
+  }
+  foreach ($keysToRemove as $key) {
+    unset($arr[$key]);
+  }
+  return $arr;
+}
+
+$arr = ['foo', 'bar', 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [0] => foo [2] => baz )
+
+//This solution first identifies keys to remove, then removes them in a second loop avoiding the index skipping issue.
